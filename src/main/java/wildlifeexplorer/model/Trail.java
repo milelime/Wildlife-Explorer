fc25803bfa3cb4ce702fd4d7f2d6bb9b7e0069bc
Alexey Ayzin <alexey.ayzin@gmail.com>
2026-04-27 16:01:48 -0400

    Update: Move Files to correct place and align with notes

--- src/main/java/wildlifeexplorer/model/Trail.java
+++ src/main/java/wildlifeexplorer/model/Trail.java
@@ -1,4 +1,83 @@
 package wildlifeexplorer.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Trail {
+    private String name;
+    private int id;
+    private double length;
+    private double elevation;
+    private double rating;
+    private List<String> pictures;
+    private List<Wildlife> wildlifeList;
+
+    private double totalRating;
+    private int numRating;
+
+    public Trail(String name, int id, double length, double elevation) {
+        this.name = name;
+        this.id = id;
+        this.length = length;
+        this.elevation = elevation;
+        this.rating = 0.0;
+        this.pictures = new ArrayList<>();
+        this.wildlifeList = new ArrayList<>();
+        this.totalRating = 0.0;
+        this.numRating = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public double getElevation() {
+        return elevation;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public List<String> getPictures() {
+        return pictures;
+    }
+
+    public List<Wildlife> getWildlifeList() {
+        return wildlifeList;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public void setElevation(double elevation) {
+        this.elevation = elevation;
+    }
+
+    public void addPicture(String picture) {
+        pictures.add(picture);
+    }
+
+    public void addWildlife(Wildlife w) {
+        wildlifeList.add(w);
+    }
+
+    public void updateRating(double newRating) {
+        totalRating += newRating;
+        numRating++;
+        rating = totalRating / numRating;
+    }
 }
--- src/main/java/wildlifeexplorer/service/TrailService.java
+++ src/main/java/wildlifeexplorer/service/TrailService.java
@@ -1,4 +1,71 @@
 package wildlifeexplorer.service;
 
+import java.util.List;
+import wildlifeexplorer.data.TrailList;
+import wildlifeexplorer.model.Trail;
+import wildlifeexplorer.model.Wildlife;
+
 public class TrailService {
+    private TrailList repository;
+
+    public TrailService(TrailList repository) {
+        this.repository = repository;
+    }
+
+    public void createTrail(String name, int id, double length, double elevation) {
+        Trail trail = new Trail(name, id, length, elevation);
+        repository.addTrail(trail);
+        repository.saveFile();
+    }
+
+    public void editTrail(String name, int id, double length, double elevation) {
+        Trail trail = repository.getTrailById(id);
+
+        if (trail != null) {
+            trail.setName(name);
+            trail.setLength(length);
+            trail.setElevation(elevation);
+            repository.saveFile();
+        } else {
+            System.out.println("Trail not found");
+        }
+    }
+
+    public void saveTrail() {
+        repository.saveFile();
+    }
+
+    public void rateTrail(int id, double rating) {
+        Trail trail = repository.getTrailById(id);
+
+        if (trail != null) {
+            trail.updateRating(rating);
+            repository.saveFile();
+        } else {
+            System.out.println("Trail not found");
+        }
+    }
+
+    public Trail getTrail(int id) {
+        return repository.getTrailById(id);
+    }
+
+    public void addWildlife(int trailId, Wildlife wildlife) {
+        Trail trail = repository.getTrailById(trailId);
+
+        if (trail != null) {
+            trail.addWildlife(wildlife);
+            repository.saveFile();
+        } else {
+            System.out.println("Trail not found");
+        }
+    }
+
+    public List<Trail> searchTrail(String name) {
+        return repository.searchTrail(name);
+    }
+
+    public List<Trail> viewTrails() {
+        return repository.viewTrail();
+    }
 }
